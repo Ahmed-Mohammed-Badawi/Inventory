2299a2da4580362038413a2f53f3cb565d201935
Ahmed Mohammed <AhmedIsmail73990@azhar.edu.eg>
2022-12-30 10:11:09 +0200

    Add Result Input

--- components/Scanner/App.jsx
+++ components/Scanner/App.jsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 import Html5QrcodePlugin from "./Html5QrcodePlugin";
 
@@ -12,6 +13,10 @@ class App extends React.Component {
         };
     }
 
+    updateTheStateHandler(event) {
+        this.setState({ text: event.target.value });
+    }
+
     render() {
         return (
             <div className='Scanner'>
@@ -22,8 +27,35 @@ class App extends React.Component {
                     qrCodeSuccessCallback={this.onNewScanResult}
                 />
 
-                <div className="ResultCode">
-                    {this.state.text}
+                <div className='ResultCode'>
+                    <div className='InputContainer'>
+                        <label htmlFor='CodeInput_label'>
+                            <Image
+                                src={"/Icons/code.svg"}
+                                width={20}
+                                height={20}
+                                alt={"Code Icon"}
+                            />
+                            <p>Code</p>
+                        </label>
+                        <div className='Input__Button'>
+                            <input
+                                id='CodeInput'
+                                type='text'
+                                placeholder='Scan for a code or type one'
+                                value={this.state.text}
+                                onInput={(e) => this.updateTheStateHandler(e)}
+                            />
+                            <button>
+                                <Image
+                                    src={"/Icons/SubmitLogin_Icon.svg"}
+                                    width={18}
+                                    height={18}
+                                    alt={"Submit Icon"}
+                                />
+                            </button>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
